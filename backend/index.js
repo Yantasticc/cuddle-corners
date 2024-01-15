730c033ed93db113eaa334bf12cb2bd89f30f783
Yantasticc <yashkalamkhede2002@gmail.com>
2024-01-15 19:22:08 +0530

    NodeJs MongoDB CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
 const app = express()
 dotenv.config()
 
@@ -14,13 +19,22 @@ const connect = async ()=> {
     }
 } 
 
-mongoose.connection.on("disconnected", () => {
-    console.log("Disconnected mongoDB Atlas");
-})
+// mongoose.connection.on("disconnected", () => {
+//     console.log("Disconnected mongoDB Atlas");
+// })
 
-mongoose.connection.on("connected", () => {
-    console.log("Connected mongoDB Atlas");
-})
+// mongoose.connection.on("connected", () => {
+//     console.log("Connected mongoDB Atlas");
+// })
+
+// middlewares
+
+app.use(express.json())
+
+app.use("/backend/auth", authRoute)
+app.use("/backend/users", usersRoute)
+app.use("/backend/hotels", hotelsRoute)
+app.use("/backend/rooms", roomsRoute)
 
 app.listen(8000, ()=> {
     connect() 
