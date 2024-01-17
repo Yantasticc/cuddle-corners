c1387e547f873747c37fc706ad13029b01be69a8
Yantasticc <yashkalamkhede2002@gmail.com>
2024-01-17 12:48:43 +0530

    register | login

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,17 @@ app.use("/backend/users", usersRoute)
 app.use("/backend/hotels", hotelsRoute)
 app.use("/backend/rooms", roomsRoute)
 
+app.use((err,req,res,next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
 app.listen(8000, ()=> {
     connect() 
     console.log("Connected to Backend")
